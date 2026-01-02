45e1e8029b7ac77843b0eb9215fc47d632c90d08
wrlaxodt <wrlaxodt@gmail.com>
2026-01-02 10:06:40 +0900

    refactor: remove default region

--- trainer/src/utils/s3.py
+++ trainer/src/utils/s3.py
@@ -4,7 +4,7 @@ import os
 def upload_to_s3(local_path: str, s3_key: str):
     s3 = boto3.client(
         "s3",
-        region_name=os.environ.get("AWS_DEFAULT_REGION", "ap-northeast-2"),
+        region_name=os.environ.get("AWS_DEFAULT_REGION"),
     )
     bucket = os.environ["S3_BUCKET_NAME"]
 
